8683b22ad7ba21ba7780587d2a05892e6bd6a243
Diego Gaspar <diegogaspardacruz1@gmail.com>
2024-06-19 13:34:21 -0300

    fix: update base url for test

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'https://mobiauto-frontend-challenge.vercel.app',
     viewportWidth: 1360,
     viewportHeight: 720,
     setupNodeEvents(on, config) {
