fbf105d697b339eec188727d2131ecdba4f3ff24
Diego Gaspar <diegogaspardacruz1@gmail.com>
2024-06-19 13:58:12 -0300

    refactor: remove api key

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,4 @@
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL_FIPE_API!
-const API_KEY = process.env.NEXT_PUBLIC_FIPE_API_KEY!
 
 export const api = async <T = unknown>(
   input: RequestInfo | URL,
@@ -9,7 +8,6 @@ export const api = async <T = unknown>(
     ...init,
     headers: {
       ...init?.headers,
-      'X-Subscription-Token': API_KEY,
     },
   })
 
